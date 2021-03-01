1de1244a2fb41b7abc02cfe1cfaf0ae7bdbdfa9c
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-01 10:51:25 +0100

    6. navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog/">Blog</Link>
+        </li>
+        <li>
+          <Link to="/products/">Product</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
-import { Link } from "gatsby"
 import React from "react"
+import Navbar from "../component/Navbar"
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link to="/blog/">blog page</Link>
-      </div>
-      <div>
-        <a href="https://www.gatsby.org">org</a>
-      </div>
+      <Navbar />
       Hello world!
     </div>
   )
