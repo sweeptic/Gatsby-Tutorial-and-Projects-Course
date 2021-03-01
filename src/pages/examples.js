32bd8ec96584589653fe54330f54573e41f50c8d
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-01 15:02:42 +0100

    27. pagequery

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,9 +1,17 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 
-const examples = () => {
+const examples = ({ data }) => {
+  // console.log(props)
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
+
   return (
     <Layout>
       <div>
@@ -11,9 +19,26 @@ const examples = () => {
         <p>hello from examples</p>
         <Header />
         <HeaderStatic />
+        <h5>author: {author}</h5>
       </div>
     </Layout>
   )
 }
+
+export const data = graphql`
+  query MyQuery {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 
 export default examples
