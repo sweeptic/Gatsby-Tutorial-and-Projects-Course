f87d92d1568a50a60edc28c2a1b1b1dfdaaf0908
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-01 13:30:51 +0100

    17. site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,16 @@
 
 module.exports = {
   /* Your site config here */
+
+  siteMetadata: {
+    title: "Gatsby Tutorial",
+    description: "some random description",
+    author: "@johndoe",
+    data: ["item 1", "item 2"],
+    person: { name: "peter", age: 32 },
+  },
+  // data: ["item 1", "item 2"],
+
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
