0fbf273d74187563b95f103d1f22233e593b9222
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-01 10:45:08 +0100

    5. link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div>
+        <Link to="/blog/">blog page</Link>
+      </div>
+      <div>
+        <a href="https://www.gatsby.org">org</a>
+      </div>
+      Hello world!
+    </div>
+  )
 }
