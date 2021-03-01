3476980fcc9584a433669eac6323a100962d3e39
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-01 14:31:27 +0100

    23. useStaticQuery and graphql

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,71 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          data
-          description
-          person {
-            age
-            name
-          }
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        author
+        data
+        description
+        person {
+          age
+          name
         }
       }
     }
-  `)
-  // return <pre>{JSON.stringify(data, null, 4)}</pre>
+  }
+`
+
+const Header = () => {
+  const {
+    site: {
+      siteMetadata: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
-      <h2>{data.site.siteMetadata.person.age}</h2>
+      {/* <h1>title: {data.site.siteMetadata.title}</h1>
+      <h1>name: {data.site.siteMetadata.person.name}</h1> */}
+      <h1>title: {title}</h1>
+      <h1>name: {name}</h1>
     </div>
   )
 }
 
-export default ComponentName
+export default Header
+
+// import React from "react"
+// import { useStaticQuery, graphql } from "gatsby"
+
+// const ComponentName = () => {
+//   const data = useStaticQuery(graphql`
+//     {
+//       site {
+//         siteMetadata {
+//           author
+//           data
+//           description
+//           person {
+//             age
+//             name
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   // return <pre>{JSON.stringify(data, null, 4)}</pre>
+//   return (
+//     <div>
+//       <h2>{data.site.siteMetadata.person.name}</h2>
+//       <h2>{data.site.siteMetadata.person.age}</h2>
+//     </div>
+//   )
+// }
+
+// export default ComponentName
