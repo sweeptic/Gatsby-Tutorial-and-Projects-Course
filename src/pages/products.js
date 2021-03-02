1f1aa8d08f322f4187e2deae4cf1007d7c744473
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-02 14:41:07 +0100

    49. products query

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,22 +1,23 @@
 import React from "react"
-import Layout from "../components/layout"
-import styles from "../components/products.module.css"
+import { graphql } from "gatsby"
 
-const products = () => {
-  return (
-    <Layout>
-      <div className={styles.page}>
-        <h1>this is our products page!</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
-          doloremque ut, voluptate magni provident facere numquam accusantium!
-          Nihil saepe reprehenderit optio necessitatibus aut provident vel quod
-          amet. Veritatis cupiditate quasi temporibus quaerat facilis voluptate
-          officiis neque facere, ullam officia quam!
-        </p>
-      </div>
-    </Layout>
-  )
-}
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
-export default products
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        price
+        title
+        slug
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ComponentName
